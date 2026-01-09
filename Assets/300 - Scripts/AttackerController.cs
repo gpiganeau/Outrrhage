0f795959428882cd8c02fc39ce6c9ca3e0e91cbb
gpiganeau <gregoirepiganeau@hotmail.fr>
2026-01-09 17:55:39 +0900

    structure creation

---
+++ Assets/300 - Scripts/AttackerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerController : MonoBehaviour
+{
+    List<SkillStrategy> activeSkillStrategies; 
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/300 - Scripts/CharacterData.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterData
+{
+    public List<SkillData> skillData;
+        
+}
