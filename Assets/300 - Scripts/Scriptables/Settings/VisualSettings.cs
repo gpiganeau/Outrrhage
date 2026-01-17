dd2e29fd44ad161ffdf969e0b3dd5b91cd32c885
Exyde <54008677+Exyde@users.noreply.github.com>
2026-01-17 18:34:14 +0100

    Visual Palette Datas

--- Assets/300 - Scripts/Scriptables/Settings/VisualSettings.cs
+++ Assets/300 - Scripts/Scriptables/Settings/VisualSettings.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Visual Settings", menuName = "Scriptable Objects/Settings/Visuals Settings")]
 public class VisualSettings : ScriptableObject
 {
-    [SerializeField] ColorPalette _bloodPalette;
+    [SerializeField] ColorPalette _bloodPalette, _techPalette, _naturePalette, _cityPalette;
     public ColorPalette BloodPalette => _bloodPalette;
+    public ColorPalette TechPalette => _techPalette;
+    public ColorPalette NaturePalette => _cityPalette;
+    public ColorPalette CityPalette => _cityPalette;
 }
 
 [System.Serializable]
@@ -12,5 +15,5 @@ public class ColorPalette
 {
     [SerializeField] string Name;
     [SerializeField] Texture2D Ramp;
-    [SerializeField] Color Main, Secondary, Ternary;
+    [SerializeField][ColorUsage(true, true)] Color Main, Secondary, Ternary;
 };
