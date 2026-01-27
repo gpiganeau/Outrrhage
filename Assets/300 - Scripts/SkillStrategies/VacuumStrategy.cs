0d704c73d56748a2240ac328161572ee5b23bb3c
Exyde <54008677+Exyde@users.noreply.github.com>
2026-01-27 12:29:09 +0100

    v1 Vacuum Strategy Skill

---
+++ Assets/300 - Scripts/SkillStrategies/VacuumStrategy.cs
@@ -0,0 +1,31 @@
+﻿using DG.Tweening;
+
+public class VacuumStrategy: SkillStrategy
+{
+    public override bool Call(MovementController movementController)
+    {
+        if (!base.Call(movementController)) return false;
+
+        movementController.SetImmobilized(true, "Vacuum");
+        parentController.SetSkillsDisabled(true, "Vacuum");
+
+        var riel = GameManager.Instance.Riel;
+        var drops = FindObjectsByType<BloodDrop>(UnityEngine.FindObjectsSortMode.None);
+
+        foreach (BloodDrop d in drops)
+        {
+            d.Attract(riel.gameObject);
+        }
+        
+        DOVirtual.DelayedCall(SettingsManager.Instance.GameplaySettings.baseMinTimeBetweenSkills, () =>
+        {
+            parentController.SetSkillsDisabled(false, "Vacuum");
+        });
+        DOVirtual.DelayedCall(SettingsManager.Instance.GameplaySettings.baseStaticTimeOnSkillUse, () =>
+        {
+            movementController.SetImmobilized(false, "Vacuum");
+        });
+        PutInCooldown();
+        return true;
+    }
+}
