7da548c024a6fc409e3642007a98d82f50abb283
Exyde <54008677+Exyde@users.noreply.github.com>
2026-02-15 10:03:05 +0100

    Safe Zone in Spawn

--- Assets/300 - Scripts/Levels/RespawnPoint.cs
+++ Assets/300 - Scripts/Levels/RespawnPoint.cs
@@ -1,8 +1,46 @@
 using UnityEngine;
 
+
+[RequireComponent(typeof(SphereCollider))]
 public class RespawnPoint : MonoBehaviour
 {
     public enum RespawnType { LevelStart, Checkpoint }
 
     public RespawnType respawnType = RespawnType.Checkpoint;
+
+    [SerializeField] private float _safeRadius = 5f;
+    private SphereCollider _collider;
+    private ParticleSystem _particleSystem;
+    private void Awake()
+    {
+        _collider = GetComponent<SphereCollider>();
+        _collider.isTrigger = true;
+        _collider.radius = _safeRadius;
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.TryGetComponent<CharacterComponent>(out var character))
+        {
+            CharacterComponent.Blood.Regain(100);
+        }
+    }
+    void OnValidate()
+    {
+        if (_collider == null) _collider = GetComponent<SphereCollider>();
+        _collider.isTrigger = true;
+        _collider.radius = _safeRadius;
+        _particleSystem = GetComponentInChildren<ParticleSystem>();
+        if (_particleSystem != null)
+        {
+            var shapeModule = _particleSystem.shape;
+            shapeModule.shapeType = ParticleSystemShapeType.Sphere;
+            shapeModule.radius = _safeRadius;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _safeRadius);
+    }
 }
