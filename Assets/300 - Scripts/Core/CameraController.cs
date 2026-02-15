6ef576fd72df991a1f6834d19df519f9432a7d2e
Exyde <54008677+Exyde@users.noreply.github.com>
2026-02-15 11:26:40 +0100

    Update Camera with meh tween, waiting for cinemachine

--- Assets/300 - Scripts/Core/CameraController.cs
+++ Assets/300 - Scripts/Core/CameraController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class CameraController : MonoBehaviour
 {
@@ -7,22 +8,34 @@ public class CameraController : MonoBehaviour
         Linear,
         Geometric,
     }
-
     [SerializeField] private TargetFollowMode followMode;
     [SerializeField] private Camera _self;
     [SerializeField] private Transform target;
     [SerializeField] private bool forceRefresh = false;
-
+    
+    [Header("Transition Settings")]
+    [SerializeField] private float _transitionDuration = 0.5f;
+    [SerializeField] private Ease _transitionEase = Ease.OutCubic;
+    
     private MovementController targetMovementController;
     private bool usePrediction;
-
     private Vector3 targetPosition;
-
+    
     public Vector3 Up => transform.up;
     public Vector3 Forward => transform.forward;
     public Vector3 Right => transform.right;
-
+    
     private CameraSettings defaultSettings;
+    
+    #region Runtime Tweener Values
+    private float _currentFollowDistance;
+    private float _currentPredictionRatio;
+    private float _currentLinearFollowSpeed;
+    private float _currentGeometricFollowSpeed;
+    
+    private Sequence settingsTweenSequence;
+
+    #endregion
 
     private void Start()
     {
@@ -30,11 +43,20 @@ public class CameraController : MonoBehaviour
         {
             _self = GetComponent<Camera>();
         }
-
         defaultSettings = SettingsManager.Instance.CameraSettings;
-        ResetCameraSettings();
+        
+        InitializeRuntimeValues(defaultSettings);
+        ResetCameraSettings(0f); 
     }
 
+    private void InitializeRuntimeValues(CameraSettings settings)
+    {
+        _currentFollowDistance = settings.cameraFollowDistance;
+        _currentPredictionRatio = settings.cameraPredictionRatio;
+        _currentLinearFollowSpeed = settings.cameraLinearFollowSpeed;
+        _currentGeometricFollowSpeed = settings.cameraGeometricFollowSpeed;
+    }
+    
     public void SetTarget(Transform newTarget)
     {
         target = newTarget;
@@ -45,54 +67,115 @@ public class CameraController : MonoBehaviour
         else
             usePrediction = false;
     }
-
-    public void SetCameraSettings(CameraSettings cameraSettings)
+    
+    public void SetCameraSettings(CameraSettings cameraSettings, float? customDuration = null)
     {
-        SettingsManager.Instance.SetCameraSettings(cameraSettings);
-        RecalculateFOVAndRotation();
+        float duration = customDuration ?? _transitionDuration;
+        TweenToSettings(cameraSettings, duration);
     }
-    public void ResetCameraSettings()
+    
+    public void ResetCameraSettings(float? customDuration = null)
     {
-        SettingsManager.Instance.SetCameraSettings(defaultSettings);
-        RecalculateFOVAndRotation();
+        float duration = customDuration ?? _transitionDuration;
+        TweenToSettings(defaultSettings, duration);
     }
+    
+    private void TweenToSettings(CameraSettings newSettings, float duration)
+    {
+        settingsTweenSequence?.Kill();
+        settingsTweenSequence = DOTween.Sequence();
+            
+        // FOV
+        settingsTweenSequence.Join(
+            DOTween.To(() => _self.fieldOfView, x => _self.fieldOfView = x, newSettings.cameraFOV, duration)
+        );
+    
+        // Rotation
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation = Quaternion.Euler(newSettings.cameraAngleVert, newSettings.cameraAngleSide, 0);
+        settingsTweenSequence.Join(
+            DOTween.To(() => 0f, x => transform.rotation = Quaternion.Slerp(startRotation, targetRotation, x), 1f, duration)
+        );
 
+        // Follow Distance
+        settingsTweenSequence.Join(
+            DOTween.To(() => _currentFollowDistance, 
+                x => _currentFollowDistance = x, 
+                newSettings.cameraFollowDistance, 
+                duration)
+        );
+
+        // Prediction Ratio
+        settingsTweenSequence.Join(
+            DOTween.To(() => _currentPredictionRatio, 
+                x => _currentPredictionRatio = x, 
+                newSettings.cameraPredictionRatio, 
+                duration)
+        );
+
+        // Linear Follow Speed
+        settingsTweenSequence.Join(
+            DOTween.To(() => _currentLinearFollowSpeed, 
+                x => _currentLinearFollowSpeed = x, 
+                newSettings.cameraLinearFollowSpeed, 
+                duration)
+        );
+
+        // Geometric Follow Speed
+        settingsTweenSequence.Join(
+            DOTween.To(() => _currentGeometricFollowSpeed, 
+                x => _currentGeometricFollowSpeed = x, 
+                newSettings.cameraGeometricFollowSpeed, 
+                duration)
+        );
+
+        settingsTweenSequence.SetEase(_transitionEase);
+        settingsTweenSequence.OnComplete(() => 
+        {
+            SettingsManager.Instance.SetCameraSettings(newSettings);
+        });
+    }
+    
     private void RecalculateFOVAndRotation()
     {
         var settings = SettingsManager.Instance.CameraSettings;
         _self.fieldOfView = settings.cameraFOV;
         transform.rotation = Quaternion.Euler(settings.cameraAngleVert, settings.cameraAngleSide, 0);
     }
-
+    
     void Update()
     {
         if (target == null) return;
-
-        var settings = SettingsManager.Instance.CameraSettings;
-
-        targetPosition = target.position - transform.forward * settings.cameraFollowDistance;
+        
+        targetPosition = target.position - transform.forward * _currentFollowDistance;
+        
         if(usePrediction)
         {
-            targetPosition += targetMovementController.Velocity 
-                * settings.cameraPredictionRatio;
+            targetPosition += targetMovementController.Velocity * _currentPredictionRatio;
         }
+        
         switch (followMode)
         {
             case TargetFollowMode.Linear:
-                transform.position = Vector3.MoveTowards(transform.position, targetPosition, settings.cameraLinearFollowSpeed * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, _currentLinearFollowSpeed * Time.deltaTime);
                 break;
             case TargetFollowMode.Geometric:
-
-                Vector3 futureMovement = (targetPosition - transform.position) * Mathf.Clamp01(settings.cameraGeometricFollowSpeed);
+                Vector3 futureMovement = (targetPosition - transform.position) * Mathf.Clamp01(_currentGeometricFollowSpeed);
                 if (futureMovement.sqrMagnitude > 0.05f)
                 {
                     transform.position += futureMovement * Time.deltaTime;
                 }
                 break;
         }
+        
         if (forceRefresh)
         {
            RecalculateFOVAndRotation();
         }
+    }
+
+    private void OnDestroy()
+    {
+        settingsTweenSequence?.Kill();
     }
 }
