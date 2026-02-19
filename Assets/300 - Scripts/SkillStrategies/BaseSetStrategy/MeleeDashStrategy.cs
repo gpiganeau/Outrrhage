0aae8de3e2d66ab90e7e8a795bdfc47b4cdd80b7
Malotrue <115473836+Malotrue@users.noreply.github.com>
2026-02-19 12:11:36 +0100

    Test EnemyBull

---
+++ Assets/300 - Scripts/SkillStrategies/BaseSetStrategy/MeleeDashStrategy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeDashStrategy: SkillStrategy
+{
+    public override void Initialize(SkillsController parent, SkillData skillData)
+    {
+        base.Initialize(parent, skillData);
+    }
+
+	public override bool Call(MovementController movementController, Team team)
+	{
+        if (!base.Call(movementController, team)) return false;
+        
+		movementController.AnimController?.Trigger(_storedSkillData.AnimationKey);
+        movementController.Dash(movementController.GetFacingDirection(), _storedSkillData.movementDistance, _storedSkillData.movementDuration, _storedSkillData.ignoreCollisions);
+        PutInCooldown();
+        return true;
+    }
+}
