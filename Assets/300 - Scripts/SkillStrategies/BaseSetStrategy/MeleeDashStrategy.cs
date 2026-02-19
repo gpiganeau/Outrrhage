0c2c324df3035c20b3a5ba2e85a329edbf623149
Malotrue <115473836+Malotrue@users.noreply.github.com>
2026-02-19 12:53:24 +0100

    Test attaque ennemi au dash

--- Assets/300 - Scripts/SkillStrategies/BaseSetStrategy/MeleeDashStrategy.cs
+++ Assets/300 - Scripts/SkillStrategies/BaseSetStrategy/MeleeDashStrategy.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class MeleeDashStrategy: SkillStrategy
 {
@@ -8,13 +9,80 @@ public class MeleeDashStrategy: SkillStrategy
         base.Initialize(parent, skillData);
     }
 
-	public override bool Call(MovementController movementController, Team team)
-	{
+    private bool hasHitATarget = false;
+    public BloodDrop bloopDropPrefab;
+
+    MovementController cachedController;
+
+    public override bool Call(MovementController movementController, Team team)
+    {
         if (!base.Call(movementController, team)) return false;
-        
-		movementController.AnimController?.Trigger(_storedSkillData.AnimationKey);
+
+        hasHitATarget = false;
+        cachedController = movementController;
+        Vector3 firingDirection = movementController.GetFacingDirection();
+
+        movementController.AnimController?.Trigger(_storedSkillData.AnimationKey);
         movementController.Dash(movementController.GetFacingDirection(), _storedSkillData.movementDistance, _storedSkillData.movementDuration, _storedSkillData.ignoreCollisions);
         PutInCooldown();
+
+        movementController.SetImmobilized(true, "MeleeDashAttack");
+        parentController.SetSkillsDisabled(true, "MeleeDashAttack");
+
+        UseAimAssist(ref firingDirection, _storedSkillData.AimAssistRatio, team);
+
+        Vector3 dashTarget = movementController.transform.position + firingDirection * 1.5f;
+        //movementController.transform.DOMove(dashTarget, 0.15f).SetEase(Ease.OutQuad);
+        movementController.transform.DOMove(dashTarget, 0.1f).SetEase(Ease.OutCubic).OnComplete(() =>
+        {
+            ProjectileData projectileData = new ProjectileData()
+            {
+                startingPosition = movementController.transform.position + 2f * firingDirection,
+                origin = movementController.transform.position,
+                Damage = _storedSkillData.ProjectileDamage[0],
+                Lifetime = _storedSkillData.ProjectileLifetime,
+                BloodStackingAmount = _storedSkillData.BloodStackingAmount,
+                Team = team,
+            };
+
+            SpawnProjectile(projectileData, 0);
+			movementController.AnimController?.Trigger(_storedSkillData.AnimationKey);
+
+
+            DOVirtual.DelayedCall(_storedSkillData.ProjectileLifetime, PostLifetimeEffects);
+            DOVirtual.DelayedCall(SettingsManager.Instance.GameplaySettings.baseMinTimeBetweenSkills, () =>
+            {
+                parentController.SetSkillsDisabled(false, "MeleeDashAttack");
+            });
+            DOVirtual.DelayedCall(SettingsManager.Instance.GameplaySettings.baseStaticTimeOnSkillUse, () =>
+            {
+                movementController.SetImmobilized(false, "MeleeDashAttack");
+            });
+            PutInCooldown();
+        });
+
+
+
         return true;
+
+    }
+
+    protected override void OnProjectileHit(Projectile projectile, DamageController damageController)
+    {
+        base.OnProjectileHit(projectile, damageController);
+        StackBlood(projectile, damageController);
+        hasHitATarget = true;
+    }
+
+    private void PostLifetimeEffects()
+    {
+        if (!hasHitATarget)
+        {
+            //Spawn bloodlet
+            var pos = cachedController.transform.position;
+            pos -= cachedController.transform.forward * 2.5f;
+            pos = pos.WithY(1);
+            Instantiate(bloopDropPrefab, pos, Quaternion.identity);
+        }
     }
 }
