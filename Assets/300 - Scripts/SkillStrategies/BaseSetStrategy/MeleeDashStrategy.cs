1a9ce1344a68e09756d56f331618863696065b47
Malotrue <115473836+Malotrue@users.noreply.github.com>
2026-02-21 17:04:31 +0100

    Revert "Chgt Enemy Blood Wave"
    This reverts commit fc84ca36e8325ebbf6ebcd6972a6fff5829a1e1d.

--- Assets/300 - Scripts/SkillStrategies/BaseSetStrategy/MeleeDashStrategy.cs
+++ Assets/300 - Scripts/SkillStrategies/BaseSetStrategy/MeleeDashStrategy.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using DG.Tweening;
 
-public class MeleeDashStrategy : SkillStrategy
+public class MeleeDashStrategy: SkillStrategy
 {
     public override void Initialize(SkillsController parent, SkillData skillData)
     {
@@ -16,23 +16,14 @@ public class MeleeDashStrategy : SkillStrategy
 
     public override bool Call(MovementController movementController, Team team)
     {
-        if (!base.Call(movementController, team)) 
-            return false;
+        if (!base.Call(movementController, team)) return false;
 
         hasHitATarget = false;
         cachedController = movementController;
-
         Vector3 firingDirection = movementController.GetFacingDirection();
 
         movementController.AnimController?.Trigger(_storedSkillData.AnimationKey);
-
-        movementController.Dash(
-            movementController.GetFacingDirection(),
-            _storedSkillData.movementDistance,
-            _storedSkillData.movementDuration,
-            _storedSkillData.ignoreCollisions
-        );
-
+        movementController.Dash(movementController.GetFacingDirection(), _storedSkillData.movementDistance, _storedSkillData.movementDuration, _storedSkillData.ignoreCollisions);
         PutInCooldown();
 
         movementController.SetImmobilized(true, "MeleeDashAttack");
@@ -41,44 +32,39 @@ public class MeleeDashStrategy : SkillStrategy
         UseAimAssist(ref firingDirection, _storedSkillData.AimAssistRatio, team);
 
         Vector3 dashTarget = movementController.transform.position + firingDirection * 1.5f;
-
         //movementController.transform.DOMove(dashTarget, 0.15f).SetEase(Ease.OutQuad);
-
-        movementController.transform
-            .DOMove(dashTarget, 0.1f)
-            .SetEase(Ease.OutCubic)
-            .OnComplete(() =>
+        movementController.transform.DOMove(dashTarget, 0.1f).SetEase(Ease.OutCubic).OnComplete(() =>
+        {
+            ProjectileData projectileData = new ProjectileData()
             {
-                ProjectileData projectileData = new ProjectileData()
-                {
-                    startingPosition = movementController.transform.position + 2f * firingDirection,
-                    origin = movementController.transform.position,
-                    Damage = _storedSkillData.ProjectileDamage[0],
-                    Lifetime = _storedSkillData.ProjectileLifetime,
-                    BloodStackingAmount = _storedSkillData.BloodStackingAmount,
-                    Team = team,
-                };
+                startingPosition = movementController.transform.position + 2f * firingDirection,
+                origin = movementController.transform.position,
+                Damage = _storedSkillData.ProjectileDamage[0],
+                Lifetime = _storedSkillData.ProjectileLifetime,
+                BloodStackingAmount = _storedSkillData.BloodStackingAmount,
+                Team = team,
+            };
 
-                SpawnProjectile(projectileData, 0);
+            SpawnProjectile(projectileData, 0);
+			movementController.AnimController?.Trigger(_storedSkillData.AnimationKey);
 
-                movementController.AnimController?.Trigger(_storedSkillData.AnimationKey);
 
-                DOVirtual.DelayedCall(_storedSkillData.ProjectileLifetime, PostLifetimeEffects);
-
-                DOVirtual.DelayedCall(
-                    SettingsManager.Instance.GameplaySettings.baseMinTimeBetweenSkills,
-                    () => parentController.SetSkillsDisabled(false, "MeleeDashAttack")
-                );
-
-                DOVirtual.DelayedCall(
-                    SettingsManager.Instance.GameplaySettings.baseStaticTimeOnSkillUse,
-                    () => movementController.SetImmobilized(false, "MeleeDashAttack")
-                );
-
-                PutInCooldown();
+            DOVirtual.DelayedCall(_storedSkillData.ProjectileLifetime, PostLifetimeEffects);
+            DOVirtual.DelayedCall(SettingsManager.Instance.GameplaySettings.baseMinTimeBetweenSkills, () =>
+            {
+                parentController.SetSkillsDisabled(false, "MeleeDashAttack");
             });
+            DOVirtual.DelayedCall(SettingsManager.Instance.GameplaySettings.baseStaticTimeOnSkillUse, () =>
+            {
+                movementController.SetImmobilized(false, "MeleeDashAttack");
+            });
+            PutInCooldown();
+        });
+
+
 
         return true;
+
     }
 
     protected override void OnProjectileHit(Projectile projectile, DamageController damageController)
@@ -92,11 +78,10 @@ public class MeleeDashStrategy : SkillStrategy
     {
         if (!hasHitATarget)
         {
-            // Spawn bloodlet
+            //Spawn bloodlet
             var pos = cachedController.transform.position;
             pos -= cachedController.transform.forward * 2.5f;
             pos = pos.WithY(1);
-
             Instantiate(bloopDropPrefab, pos, Quaternion.identity);
         }
     }
