fb8b3d69ccf8841a2b0019bdf85c85c2bab3e9d1
Exyde <54008677+Exyde@users.noreply.github.com>
2026-02-23 19:57:47 +0100

    Rnadom Music

--- Assets/300 - Scripts/AudioManager.cs
+++ Assets/300 - Scripts/AudioManager.cs
@@ -1,13 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum SoundGroup { Ambient, SFX, Music, Bark, Global }
 public class AudioManager : MonoBehaviour
 {
-
-
     public static AudioManager Instance;
 
     private AudioSettings settings;
+
+
+    [SerializeField] public List<AudioClip> Songs;
+    AudioSource musicSource;
 
     public void Awake()
     {
@@ -18,6 +21,15 @@ public class AudioManager : MonoBehaviour
     void Start()
     {
         settings = SettingsManager.Instance.AudioSettings;
+        musicSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void PlayRandomMusic()
+    {
+        musicSource.clip = Songs.Random();
+        musicSource.volume = settings.musicVolume;
+        musicSource.loop = true;
+        musicSource.Play();
     }
 
     public void PlayClipAtPoint(AudioClip clip, Vector3 pos, SoundGroup group = SoundGroup.Global)
