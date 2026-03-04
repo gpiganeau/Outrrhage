60f42c37f249aa5b29cf1e4adea9c0bed7384e29
Exyde <54008677+Exyde@users.noreply.github.com>
2026-03-04 17:59:17 +0100

    Setup for new brain

--- Assets/300 - Scripts/Scriptables/AIAttackStrategies/AttackStrategyDistantProjectileSetupData.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Attack Strategy Distant Projectile", menuName = "Scriptable Objects/Game/AI/AS Distant Projectile")]
-public class AttackStrategyDistantProjectileSetupData: AttackStrategySetupData
-{
-}
